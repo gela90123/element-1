1761b4e7a6cbd1efe01053bfd571e9bdfe6d588d
gela90123 <149181881+gela90123@users.noreply.github.com>
2023-11-17 14:19:08 +0000

    Update ssh_connection.py

--- ssh_connection.py
+++ ssh_connection.py
@@ -1,6 +1,5 @@
 import pexpect
 import logging
-import difflib
 
 class NetworkDeviceConfigurator:
     def __init__(self, ip, username, password, enable_password):
@@ -12,6 +11,7 @@ class NetworkDeviceConfigurator:
 
     def connect(self):
         try:
+            # Establish an SSH session
             self.session = pexpect.spawn(f'ssh {self.username}@{self.ip}', encoding='utf-8', timeout=20)
             result = self.session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
 
@@ -41,160 +41,41 @@ class NetworkDeviceConfigurator:
                 return False
 
             return True
-        except Exception as e:
+        except pexpect.ExceptionPexpect as e:
             logging.error(f"Failed to establish an SSH connection: {e}")
             return False
 
-    def configure_hostname(self, new_hostname):
+    def compare_running_config_with_hardening_advice(self, hardening_advice):
         try:
-            self.session.sendline('configure terminal')
-            result = self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to enter config mode for {self.ip}')
-                return False
-
-            self.session.sendline(f'hostname {new_hostname}')
-            result = self.session.expect([fr'{new_hostname}\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to set the hostname for {self.ip}')
-                return False
-
-            self.session.sendline('exit')
-            self.session.sendline('exit')
-
-            return True
-        except Exception as e:
-            logging.error(f"Configuration failed: {e}")
-            return False
-
-    def save_running_config(self, output_file):
-        try:
+            # Retrieve running configuration
             self.session.sendline('show running-config')
-            result = self.session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to capture the running configuration for {self.ip}')
-                return False
-
+            self.session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
             running_config = self.session.before  # Get the output before the prompt
 
-            with open(output_file, 'w') as f:
-                f.write(running_config)
-
-            return True
-        except Exception as e:
-            logging.error(f"Failed to save the running configuration: {e}")
-            return False
-
-    def compare_with_hardening_advice(self):
-        try:
-            hardening_advice = """
-            en
-            conf t
-            int g0/0
-            ip address 192.168.1.1 255.255.255.0
-            no shut
-            exit
-            line vty 0 4
-            transport input all
-            login local
-            username cisco password cisco
-            ip domain-name domain.com
-            hostname R1
-            enable secret class
-            crypto key generate rsa
-            ip ssh version 2
-            """
-
-            # Debug: Print hardening advice for debugging
-            print('------------------------------------------------------')
-            print('Hardening Advice:')
-            print(hardening_advice)
-            print('------------------------------------------------------')
-
-            self.session.sendline('enable')
-            result = self.session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to enter enable mode for {self.ip}')
-                # Debug: Print the session output for troubleshooting
-                print('Failed to enter enable mode. Session output:')
-                print(self.session.before)
-                print(self.session.after)
-                return False
-
-            self.session.sendline('configure terminal')
-            result = self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to enter config mode for {self.ip}')
-                # Debug: Print the session output for troubleshooting
-                print('Failed to enter config mode. Session output:')
-                print(self.session.before)
-                print(self.session.after)
-                return False
-
-            self.session.sendline('show running-config')
-            result = self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to capture the running configuration for {self.ip}')
-                # Debug: Print the session output for troubleshooting
-                print('Failed to capture running configuration. Session output:')
-                print(self.session.before)
-                print(self.session.after)
-                return False
-
-            running_config = self.session.before
-
-            # Print running configuration for debugging
-            print('------------------------------------------------------')
-            print('Running Configuration:')
-            print(running_config)
-            print('------------------------------------------------------')
-
-            # Compare running configuration with hardening advice
-            d = difflib.Differ()
-            diff = list(d.compare(hardening_advice.splitlines(), running_config.splitlines()))
-
-            if any(line.startswith('- ') or line.startswith('+ ') for line in diff):
-                print('------------------------------------------------------')
-                print('Configuration does not match Cisco device hardening advice:')
-                for line in diff:
-                    print(line)
-                print('------------------------------------------------------')
+            # Compare configurations
+            if running_config.strip() == hardening_advice.strip():
+                print("Running configuration is in compliance with Cisco device hardening advice.")
             else:
-                print('------------------------------------------------------')
-                print('Configuration matches Cisco device hardening advice.')
-                print('------------------------------------------------------')
+                print("Running configuration does not match Cisco device hardening advice.")
 
             return True
-        except Exception as e:
-            logging.error(f"Failed to compare with hardening advice: {e}")
+        except pexpect.ExceptionPexpect as e:
+            logging.error(f"Comparison failed: {e}")
             return False
 
-    def configure_syslog(self, syslog_server_ip):
+    def enable_syslog(self, syslog_server_ip):
         try:
             self.session.sendline('configure terminal')
-            result = self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to enter config mode for {self.ip}')
-                return False
+            self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
 
             self.session.sendline(f'logging host {syslog_server_ip}')
-            result = self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
-
-            if result != 0:
-                logging.error(f'Failed to configure syslog for {self.ip}')
-                return False
+            self.session.expect([r'\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
 
             self.session.sendline('end')
+            self.session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
 
             return True
-        except Exception as e:
+        except pexpect.ExceptionPexpect as e:
             logging.error(f"Syslog configuration failed: {e}")
             return False
 
@@ -203,46 +84,40 @@ class NetworkDeviceConfigurator:
             self.session.close()
             self.session = None
 
+# Usage
 if __name__ == "__main__":
+    # Set the device and user credentials
+    ip_address = 'your_device_ip'
+    username = 'your_username'
+    password = 'your_password'
+    enable_password = 'your_enable_password'
+    syslog_server_ip = 'your_syslog_server_ip'  # Replace with your syslog server IP
+    hardening_advice = """
+    Your Cisco device hardening advice goes here.
+    Replace this multiline string with the actual advice.
+    """
+
+    # Initialize logging for error tracking
     logging.basicConfig(filename='network_device_configurator.log', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
-    ip_address = '192.168.56.101'
-    username = 'prne'
-    password = 'cisco123!'
-    password_enable = 'class123!'
-    new_hostname = 'R1'
-    syslog_server_ip = '<syslog_server_ip>'
+    # Create a NetworkDeviceConfigurator instance
+    device = NetworkDeviceConfigurator(ip_address, username, password, enable_password)
 
-    device = NetworkDeviceConfigurator(ip_address, username, password, password_enable)
-
-    if device.connect() and device.configure_hostname(new_hostname):
-        print('------------------------------------------------------')
-        print('')
-        print(f'--- Success! Connecting to {ip_address} as user {username}')
-        print(f'--- Password: {password}')
-        print(f'--- Hostname changed to: {new_hostname}')
-        print('')
-        print('------------------------------------------------------')
-
-        # Save the running configuration to a file
-        output_file = 'running_config.txt'
-        if device.save_running_config(output_file):
-            print(f'--- Running configuration saved to {output_file}')
+    if device.connect():
+        # Compare running config with hardening advice
+        if device.compare_running_config_with_hardening_advice(hardening_advice):
+            print("--- Configuration is in compliance with hardening advice.")
         else:
-            print(f'--- Failed to save running configuration.')
+            print("--- Configuration does not match hardening advice.")
 
-        # Compare running configuration with hardening advice
-        device.compare_with_hardening_advice()
-
-        # Configure syslog
-        if device.configure_syslog(syslog_server_ip):
-            print('--- Syslog configuration successful.')
+        # Enable syslog
+        if device.enable_syslog(syslog_server_ip):
+            print("--- Syslog configuration successful.")
         else:
-            print('--- Syslog configuration failed.')
+            print("--- Syslog configuration failed.")
 
-        logging.info(f"Configuration of {ip_address} successful.")
+        # Disconnect from the device
+        device.disconnect()
     else:
         print(f"Configuration of {ip_address} failed.")
         logging.error(f"Configuration of {ip_address} failed.")
-
-    device.disconnect()
